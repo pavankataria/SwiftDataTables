897e264715815b93bcac7cf4bddfd618e67d9735
Pavan Kataria <kataria.pavan@gmail.com>
2026-01-19 06:57:04 +0000

    Add DataTableAnimation enum for incremental updates
    Simple enum with .automatic and .none cases to control animation
    behavior during data updates.

---
+++ SwiftDataTables/Classes/Models/DataTableAnimation.swift
@@ -0,0 +1,13 @@
+//
+//  DataTableAnimation.swift
+//  SwiftDataTables
+//
+//  Created by Codex on 2026-xx-xx.
+//
+
+import Foundation
+
+public enum DataTableAnimation {
+    case automatic
+    case none
+}
